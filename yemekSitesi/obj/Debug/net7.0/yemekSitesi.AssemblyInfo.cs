e35903ba628da7d6bb9e09c606c6a9d0fe952c13
İlayda Yıldız <148477950+ilaydayildizz@users.noreply.github.com>
2024-12-27 09:33:27 +0300

    ilayda
    i

--- yemekSitesi/obj/Debug/net7.0/yemekSitesi.AssemblyInfo.cs
+++ yemekSitesi/obj/Debug/net7.0/yemekSitesi.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("yemekSitesi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3973d4b13b956fdb5e2f451d3ee3d23e700c64")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
->>>>>>> 4045050f624ac24de98fb3448420e13c7c2c411a
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f981a842c304327f02ed0c4ab991e11c670b559f")]
 [assembly: System.Reflection.AssemblyProductAttribute("yemekSitesi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("yemekSitesi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
